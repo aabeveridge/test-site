c565ed199470858ece064b736017e71bab0831d8
aabeveridge <aaronbev79@gmail.com>
2016-10-11 11:03:33 -0400

    updates

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,8 @@ $("#menudown").click(function(){
     $("#hiddennav").toggle();
     $("#menuup").toggle();
     $("#menudown").toggle();
-    $( "#menu" ).css('margin-bottom','-35px');
-    $( "#cover-text" ).css('margin-top','-1px');
+    $( "#menu" ).css('margin-bottom','-40px');
+    $( "#cover-text" ).css('margin-top','0px');
 });
 
 $("#menuup").click(function(){
@@ -12,4 +12,12 @@ $("#menuup").click(function(){
     $("#menuup").toggle();
     $( "#menu" ).css('margin-bottom','0px');
     $( "#cover-text" ).css('margin-top','0px');
+});
+
+$("#cover-hi").click(function(){
+    $("#hiddennav").show();
+});
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
 });
